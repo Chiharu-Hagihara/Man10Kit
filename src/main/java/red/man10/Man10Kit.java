d0af582e659df75bdc4aca92064e8645d5135d2a
mametaku <mametaku.man10@gmail.com>
2020-10-14 11:07:54 +0900

    Initial commit

---
+++ src/main/java/red/man10/Man10Kit.java
@@ -0,0 +1,398 @@
+package red.man10;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public final class Man10Kit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        // Plugin startup logic
+        //  getServer().getPluginManager().registerEvents (this,this);
+        //
+        getCommand("mkit").setExecutor(new Man10KitCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+    //      キットを削除
+    public boolean delete(Player p, String kitName){
+        String fileName = kitName;
+        File userdata = new File(Bukkit.getServer().getPluginManager().getPlugin("Man10Kit").getDataFolder(), File.separator + "Kits");
+        File f = new File(userdata, File.separator + fileName + ".yml");
+
+        if(f.delete()){
+            serverMessage(p.getDisplayName()+"はkit: "+kitName+" を削除した");
+            return false;
+        }else{
+            serverMessage(p.getDisplayName()+"はkit: "+kitName+" の削除に失敗した");
+
+            return false;
+        }
+    }
+    //      キットを保存する
+    public boolean push(Player p){
+
+        PlayerInventory inv= p.getInventory();
+        //String kitString = toBase64(inv);
+
+        String fileName = p.getUniqueId().toString();
+        File userdata = new File(Bukkit.getServer().getPluginManager().getPlugin("Man10Kit").getDataFolder(), File.separator + "Users");
+        File f = new File(userdata, File.separator + fileName + ".yml");
+        FileConfiguration data = YamlConfiguration.loadConfiguration(f);
+
+        f.delete();
+
+        if (!f.exists()) {
+            try {
+                data.set("creator", p.getName());
+                data.set("inventory",p.getInventory().getContents());
+                data.set("armor",p.getInventory().getArmorContents());
+                data.save(f);
+            } catch (IOException exception) {
+                p.sendMessage("キットの保存に失敗した");
+                exception.printStackTrace();
+                return false;
+            }
+        }
+
+        //serverMessage(""+p.getName()+ ":ユーザーデータを保存した:"+fileName);
+
+        return true;
+    }
+    //      キットを保存する
+    public boolean save(Player p, String kitName){
+
+        PlayerInventory inv= p.getInventory();
+        //  String kitString = toBase64(inv);
+
+        String fileName = kitName;
+        File userdata = new File(Bukkit.getServer().getPluginManager().getPlugin("Man10Kit").getDataFolder(), File.separator + "Kits");
+        File f = new File(userdata, File.separator + fileName + ".yml");
+        FileConfiguration data = YamlConfiguration.loadConfiguration(f);
+
+
+        if (!f.exists()) {
+            try {
+
+                data.set("creator", p.getName());
+                data.set("inventory",p.getInventory().getContents());
+                data.set("armor",p.getInventory().getArmorContents());
+                data.save(f);
+            } catch (IOException exception) {
+                p.sendMessage("キットの保存に失敗した");
+                exception.printStackTrace();
+                return false;
+            }
+        }
+
+        //serverMessage(""+p.getName()+ ":キットを保存した:"+kitName);
+
+        return true;
+    }
+
+    //      キット
+    public boolean list(Player p) {
+
+        File folder = new File(Bukkit.getServer().getPluginManager().getPlugin("Man10Kit").getDataFolder(), File.separator + "Kits");
+
+        File[] files = folder.listFiles();  // (a)
+        for (File f : files) {
+            if (f.isFile()){  // (c)
+                String filename = f.getName();
+
+                if(filename.substring(0,1).equalsIgnoreCase(".")){
+                    continue;
+                }
+
+                int point = filename.lastIndexOf(".");
+                if (point != -1) {
+                    filename =  filename.substring(0, point);
+                }
+                p.sendMessage(filename);
+            }
+        }
+
+        return true;
+    }
+    //      キットを読み込む
+    public boolean load(Player p, String kitName){
+        String fileName = kitName;
+        File userdata = new File(Bukkit.getServer().getPluginManager().getPlugin("Man10Kit").getDataFolder(), File.separator + "Kits");
+        File f = new File(userdata, File.separator + fileName + ".yml");
+        FileConfiguration data = YamlConfiguration.loadConfiguration(f);
+        if (!f.exists()) {
+            p.sendMessage("キットは存在しない:"+kitName);
+            return false;
+        }
+
+
+        Object a = data.get("inventory");
+        Object b = data.get("armor");
+
+        if(a == null || b == null){
+            p.sendMessage("No saved inventory to load");
+            return true;
+        }
+        ItemStack[] inventory = null;
+        ItemStack[] armor = null;
+        if (a instanceof ItemStack[]){
+            inventory = (ItemStack[]) a;
+        } else if (a instanceof List){
+            List lista = (List) a;
+            inventory = (ItemStack[]) lista.toArray(new ItemStack[0]);
+        }
+        if (b instanceof ItemStack[]){
+            armor = (ItemStack[]) b;
+        } else if (b instanceof List){
+            List listb = (List) b;
+            armor = (ItemStack[]) listb.toArray(new ItemStack[0]);
+        }
+        p.getInventory().clear();
+        p.getInventory().setContents(inventory);
+        p.getInventory().setArmorContents(armor);
+
+
+
+        return true;
+    }
+    //      キットを読み込む
+    public boolean pop(Player p){
+        String fileName = p.getUniqueId().toString();
+        File userdata = new File(Bukkit.getServer().getPluginManager().getPlugin("Man10Kit").getDataFolder(), File.separator + "Users");
+        File f = new File(userdata, File.separator + fileName + ".yml");
+        FileConfiguration data = YamlConfiguration.loadConfiguration(f);
+        if (!f.exists()) {
+            p.sendMessage("ユーザーデーターは存在しない:"+p.getName());
+            return false;
+        }
+
+
+        Object a = data.get("inventory");
+        Object b = data.get("armor");
+
+        if(a == null || b == null){
+            p.sendMessage("No saved inventory to load");
+            return true;
+        }
+        ItemStack[] inventory = null;
+        ItemStack[] armor = null;
+        if (a instanceof ItemStack[]){
+            inventory = (ItemStack[]) a;
+        } else if (a instanceof List){
+            List lista = (List) a;
+            inventory = (ItemStack[]) lista.toArray(new ItemStack[0]);
+        }
+        if (b instanceof ItemStack[]){
+            armor = (ItemStack[]) b;
+        } else if (b instanceof List){
+            List listb = (List) b;
+            armor = (ItemStack[]) listb.toArray(new ItemStack[0]);
+        }
+        p.getInventory().clear();
+        p.getInventory().setContents(inventory);
+        p.getInventory().setArmorContents(armor);
+
+        return true;
+    }
+
+    boolean saveUserKit(Player p, String kitName){
+
+        String kitString = toBase64(p.getInventory());
+
+
+        String fileName = kitName;
+        File userdata = new File(Bukkit.getServer().getPluginManager().getPlugin("Man10Kit").getDataFolder(), File.separator + "PlayerData");
+        File f = new File(userdata, File.separator + fileName + ".yml");
+        FileConfiguration data = YamlConfiguration.loadConfiguration(f);
+
+        //When the player file is created for the first time...
+        if (!f.exists()) {
+            try {
+
+                data.set("creator", p.getName());
+                data.set("inventory",kitString);
+
+                data.save(f);
+            } catch (IOException exception) {
+                serverMessage("キットの保存に失敗した");
+                exception.printStackTrace();
+                return false;
+            }
+        }
+
+        serverMessage("キットの保存に成功した");
+        return true;
+    }
+
+
+    //     サーバーメッセージ
+    void serverMessage(String text){
+        //command("say "+text);
+        Bukkit.getServer().broadcastMessage(text);
+    }
+    /**
+     * Converts the player inventory to a String array of Base64 strings. First string is the content and second string is the armor.
+     *
+     * @param playerInventory to turn into an array of strings.
+     * @return Array of strings: [ main content, armor content ]
+     * @throws IllegalStateException
+     */
+    public static String[] playerInventoryToBase64(PlayerInventory playerInventory) throws IllegalStateException {
+        //get the main content part, this doesn't return the armor
+        String content = toBase64(playerInventory);
+        String armor = itemStackArrayToBase64(playerInventory.getArmorContents());
+
+        return new String[] { content, armor };
+    }
+
+    /**
+     *
+     * A method to serialize an {@link ItemStack} array to Base64 String.
+     *
+     * <p />
+     *
+     * Based off of {@link #toBase64(Inventory)}.
+     *
+     * @param items to turn into a Base64 String.
+     * @return Base64 string of the items.
+     * @throws IllegalStateException
+     */
+    public static String itemStackArrayToBase64(ItemStack[] items) throws IllegalStateException {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+
+            // Write the size of the inventory
+            dataOutput.writeInt(items.length);
+
+            // Save every element in the list
+            for (int i = 0; i < items.length; i++) {
+                dataOutput.writeObject(items[i]);
+            }
+
+            // Serialize that array
+            dataOutput.close();
+            return Base64Coder.encodeLines(outputStream.toByteArray());
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to save item stacks.", e);
+        }
+    }
+
+    /**
+     * A method to serialize an inventory to Base64 string.
+     *
+     * <p />
+     *
+     * Special thanks to Comphenix in the Bukkit forums or also known
+     * as aadnk on GitHub.
+     *
+     * <a href="https://gist.github.com/aadnk/8138186">Original Source</a>
+     *
+     * @param inventory to serialize
+     * @return Base64 string of the provided inventory
+     * @throws IllegalStateException
+     */
+    public static String toBase64(Inventory inventory) throws IllegalStateException {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+
+            // Write the size of the inventory
+            dataOutput.writeInt(inventory.getSize());
+
+            // Save every element in the list
+            for (int i = 0; i < inventory.getSize(); i++) {
+                dataOutput.writeObject(inventory.getItem(i));
+            }
+
+            // Serialize that array
+            dataOutput.close();
+            return Base64Coder.encodeLines(outputStream.toByteArray());
+        } catch (Exception e) {
+            throw new IllegalStateException("Unable to save item stacks.", e);
+        }
+    }
+
+    /**
+     *
+     * A method to get an {@link Inventory} from an encoded, Base64, string.
+     *
+     * <p />
+     *
+     * Special thanks to Comphenix in the Bukkit forums or also known
+     * as aadnk on GitHub.
+     *
+     * <a href="https://gist.github.com/aadnk/8138186">Original Source</a>
+     *
+     * @param data Base64 string of data containing an inventory.
+     * @return Inventory created from the Base64 string.
+     * @throws IOException
+     */
+    public static Inventory fromBase64(String data) throws IOException {
+        try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
+            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+            Inventory inventory = Bukkit.getServer().createInventory(null, dataInput.readInt());
+
+            // Read the serialized inventory
+            for (int i = 0; i < inventory.getSize(); i++) {
+                inventory.setItem(i, (ItemStack) dataInput.readObject());
+            }
+
+            dataInput.close();
+            return inventory;
+        } catch (ClassNotFoundException e) {
+            throw new IOException("Unable to decode class type.", e);
+        }
+    }
+
+    /**
+     * Gets an array of ItemStacks from Base64 string.
+     *
+     * <p />
+     *
+     * Base off of {@link #fromBase64(String)}.
+     *
+     * @param data Base64 string to convert to ItemStack array.
+     * @return ItemStack array created from the Base64 string.
+     * @throws IOException
+     */
+    public static ItemStack[] itemStackArrayFromBase64(String data) throws IOException {
+        try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
+            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+            ItemStack[] items = new ItemStack[dataInput.readInt()];
+
+            // Read the serialized inventory
+            for (int i = 0; i < items.length; i++) {
+                items[i] = (ItemStack) dataInput.readObject();
+            }
+
+            dataInput.close();
+            return items;
+        } catch (ClassNotFoundException e) {
+            throw new IOException("Unable to decode class type.", e);
+        }
+    }
+
+
+}
---
+++ src/main/java/red/man10/Man10KitCommand.java
@@ -0,0 +1,252 @@
+package red.man10;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * Created by takatronix on 2017/03/06.
+ */
+public class Man10KitCommand implements CommandExecutor {
+    private Man10Kit plugin;
+
+    public Man10KitCommand(Man10Kit plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        ////////////////////////////////////
+        //          jail
+        ////////////////////////////////////
+        if (args[0].equalsIgnoreCase("jail")) {
+
+            if (args.length != 2) {
+                sender.sendMessage("/mkit jail [username]");
+                return false;
+            }
+            Player t = Bukkit.getPlayer(args[1]);
+            Bukkit.getLogger().info(args[1]);
+
+            if(t.isOnline() == false){
+                Bukkit.getLogger().info("player is not online");
+                sender.sendMessage(t.getName() +"はオンラインではありません");
+                return false;
+            }
+            plugin.push(t);
+            Bukkit.getLogger().info("_jail");
+            plugin.load(t,"_jail");
+            return true;
+        }
+
+
+        if (args[0].equalsIgnoreCase("push")) {
+
+
+            //    引数がある場合
+            if (args.length == 2) {
+                String name = args[1];
+                Player p = Bukkit.getPlayer(name);
+                if(p == null){
+                    sender.sendMessage(name+"はオフラインです");
+                    return false;
+                }
+                plugin.push(p);
+                sender.sendMessage(name+"のユーザーデータをバックアップしました");
+                return true;
+            }
+
+            //      ユーザーコマンド
+            if (sender instanceof Player){
+                Player p = (Player) sender;
+                plugin.push(p);
+                return true;
+            }
+
+
+
+            return true;
+        }
+        ////////////////////////////////////
+        //          set
+        ////////////////////////////////////
+        if (args[0].equalsIgnoreCase("set")) {
+
+            Bukkit.getLogger().info("set");
+            if (args.length != 3) {
+                sender.sendMessage("/mkit set [username] [KitName]");
+                return false;
+            }
+
+            Player t = Bukkit.getPlayer(args[1]);
+            if(t.isOnline() == false){
+                Bukkit.getLogger().info("player is not online");
+                sender.sendMessage(t.getName() +"はオンラインではありません");
+                return false;
+            }
+
+
+            plugin.load(t,args[2]);
+            return true;
+        }
+
+
+        if ((args[0].equalsIgnoreCase("pop")) || (args[0].equalsIgnoreCase("unjail"))) {
+            //    引数がある場合
+
+            if (args.length == 2) {
+                String name = args[1];
+                Player p = Bukkit.getPlayer(name);
+                p.sendMessage(args[0]);
+                if(p == null){
+                    sender.sendMessage(name+"はオフラインです");
+                    return false;
+                }
+                if(plugin.pop(p)){
+                    sender.sendMessage(name+"のユーザーデータを復元しました");
+                }else{
+                    sender.sendMessage(name+"のユーザーデータは存在しない");
+                }
+                return true;
+            }
+
+            //      ユーザーコマンド
+            if (sender instanceof Player){
+                Player p = (Player) sender;
+                plugin.pop(p);
+                return true;
+            }
+
+            return true;
+        }
+
+
+
+
+
+        if(sender instanceof Player)
+        {
+            Player p = (Player) sender;
+
+            //      引数がない場合
+            if (args.length < 1) {
+                showHelp(p);
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("help")) {
+                showHelp(p);
+                return true;
+            }
+
+
+            ////////////////////////////////////
+            //          save
+            ////////////////////////////////////
+            if (args[0].equalsIgnoreCase("save")) {
+
+                if (args.length <= 1) {
+                    p.sendMessage("/mkit save [KitName]");
+                    return false;
+                }
+
+                plugin.save(p,args[1]);
+                return true;
+            }
+
+            ////////////////////////////////////
+            //          Load
+            ////////////////////////////////////
+            if (args[0].equalsIgnoreCase("load")) {
+
+                if (args.length <= 1) {
+                    p.sendMessage("/mkit load [KitName]");
+                    return false;
+                }
+
+                plugin.load(p,args[1]);
+                return true;
+            }
+
+
+
+            /////////////////////////////////////
+            //          delete
+            ////////////////////////////////////
+            if (args[0].equalsIgnoreCase("delete")) {
+
+                if (args.length < 1) {
+                    p.sendMessage("/mkit delete [KitName]");
+                    return false;
+                }
+
+                plugin.delete(p,args[1]);
+                return true;
+            }
+            /////////////////////////////////////
+            //          list
+            ////////////////////////////////////
+            if (args[0].equalsIgnoreCase("list")) {
+
+                plugin.list(p);
+                return true;
+            }
+
+
+
+
+
+            showHelp(p);
+            return true;
+            // The command was executed by a player.
+        }
+        /*
+        else if(sender instanceof ConsoleCommandSender)
+        {
+            ////////////////////////////////////
+            //          set
+            ////////////////////////////////////
+            if (args[0].equalsIgnoreCase("set")) {
+                Bukkit.getLogger().info("set");
+                if (args.length <= 2) {
+                    sender.sendMessage("/mkit set [username] [KitName]");
+                    return false;
+                }
+                Player t = Bukkit.getPlayer(args[1]);
+                if(t.isOnline() == false){
+                    Bukkit.getLogger().info("player is not online");
+                    sender.sendMessage(t.getName() +"はオンラインではありません");
+                    return false;
+                }
+                plugin.load(t,args[2]);
+                return true;
+            }
+            //The command was executed from console.
+        }
+        //The command was executed from console.
+        showHelp(sender);
+*/
+        return true;
+    }
+    void showHelp(CommandSender p){
+        p.sendMessage("§e==============§d●§f●§a●§e Man10 KitPlugin §d●§f●§a●§e===============");
+        p.sendMessage("" +
+                "http://man10.red by takatronix\n" +
+                "/mkit list - List all kits\n" +
+                "/mkit load - Load a kit\n" +
+                "/mkit save - Save your inventory to kit\n" +
+                "/mkit delete - Delete a saved kit\n" +
+                "/mkit push - Push user's inventory\n" +
+                "/mkit jail - Apply Jail Kit\n" +
+                "/mkit unuail - Revert user's inventory\n" +
+                "/mkit pop - Pop user's inventory\n"
+        );
+    }
+
+}
+
+
+
